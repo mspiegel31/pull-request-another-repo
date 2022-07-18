789da5d7d91f9861f726ffffeff7787b0e02de14
Mike Spiegel <mspiegel31@gmail.com>
2022-07-18 17:49:45 +0000

    adding config

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from pr_another_repo.client import gh
 
 
 def main():
-    r = gh.get_repo("mspiegel31/dotfiles")
-    print(r)
+    original_repo = gh.get_repo("mspiegel31/dotfiles")
+    destination_repo = gh.get_repo("mspiegel31/dotfiles")
 
 
 if __name__ == "__main__":
--- pr_another_repo/settings.py
+++ pr_another_repo/settings.py
@@ -1,8 +1,25 @@
-from pydantic import BaseSettings, SecretStr
+from typing import List
+
+from pydantic import BaseSettings, SecretStr, validator
 
 
 class ActionInputs(BaseSettings):
     github_api_token: SecretStr
+    user_email: str
+    user_name: str
+    destination_repo: str
+    destination_head_branch: str
+    destination_base_branch: str = "main"
+    destination_folder: str = ""
+    pull_request_reviewers: List[str] = []
+
+    @validator("destination_head_branch")
+    def avoid_dangerous_branch_names(cls, v):
+        if v in set(["main", "master"]):
+            raise ValueError(
+                "cannot push to main or master.  Push changes to different branch"
+            )
+        return v
 
     class Config:
         # Github serves us action inputs as env vars like INPUT_{name}
