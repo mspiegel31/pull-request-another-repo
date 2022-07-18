3d2c42fa228aeadf16d2d56d54e1149421fd958b
Mike Spiegel <mspiegel31@gmail.com>
2022-07-18 19:48:17 +0000

    sketching out (untested) flow

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python
 
+import subprocess
+from pathlib import Path
+
+from github.Repository import Repository
+
 from pr_another_repo.client import gh
 from pr_another_repo.settings import settings
 
 
-def issue_pr():
-    destination_repo = gh.get_repo(settings.action_inputs.destination_repo)
+def clone_dest_repo(repo: Repository) -> Path:
+    clone_url = repo.clone_url
+    subprocess.run(["git", "clone", clone_url, "/tmp/destination_repo"])
+    return Path("/tmp/destination_repo")
+
+
+def issue_pr(repo: Repository):
+    pass
 
 
 def main():
+    destination_repo = gh.get_repo(settings.action_inputs.destination_repo)
     # clone source
     # issue commit
     # issue pr
-
-    issue_pr()
+    local_repo_location = clone_dest_repo(destination_repo)
+    issue_pr(destination_repo)
 
 
 if __name__ == "__main__":
