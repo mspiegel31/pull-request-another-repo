83a6cc7586833bd7375358442503c6e63ed5dc2f
Mike Spiegel <mspiegel31@gmail.com>
2022-09-01 13:51:03 +0000

    quality of life updates

--- pr_another_repo/settings.py
+++ pr_another_repo/settings.py
@@ -1,20 +1,21 @@
 from typing import List
 
-from pydantic import BaseSettings, EmailStr, SecretStr, validator
+from pydantic import BaseSettings, EmailStr, SecretStr, validator, DirectoryPath
 
 
 class ActionInputs(BaseSettings):
     github_api_token: SecretStr
     user_email: EmailStr = EmailStr("github-actions@users.noreply.github.com")
     user_name: str = "github-actions[bot]"
-    # TODO:: maybe use a file/directory path? https://pydantic-docs.helpmanual.io/usage/types/#pydantic-types
-    source_folder: str
+    source_folder: DirectoryPath
     destination_owner: str
     destination_repo: str
     destination_head_branch: str
     destination_base_branch: str = "main"
     destination_folder: str = ""
     pull_request_reviewers: List[str] = []
+    pull_request_title = 'Automated PR'
+    pull_request_body = ''
 
     @validator("destination_head_branch")
     def avoid_dangerous_branch_names(cls, v):
