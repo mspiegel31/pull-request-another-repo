7d716b7ad5457339132b1057104fd72f17f120cd
Mike Spiegel <mspiegel31@gmail.com>
2022-09-03 22:01:42 +0000

    adding basic tests

--- test/test_main.py
+++ test/test_main.py
@@ -4,6 +4,10 @@ import pytest
 
 @patch('main.gh', autospec=True)
 def test_fails_if_bad_repo(mock_gh):
-    mock_gh.get_repo.side_effect = Exception()
-    with pytest.raises(Exception) as err:
+    err_message = 'whups bad url'
+    mock_gh.get_repo.side_effect = Exception(err_message)
+    with pytest.raises(Exception) as exc_info:
         main()
+    
+    assert exc_info.type == Exception
+    assert exc_info.value.args[0] == err_message
